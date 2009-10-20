60183f30584572365ea8c3741963476393f962c8
Brent Pedersen <bpederse@gmail.com>
2009-10-20 14:44:07 -0700

    allow the port to be specified via the command line and allow the user to the end process with 'DONE'

--- nwalign/nwserver.py
+++ nwalign/nwserver.py
@@ -1,14 +1,15 @@
 import socket
 from nwalign import global_align
+import sys
 
-PORT = 1233
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 1233
 CHUNK = 32768
 HOST = 'localhost'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(5)
-print "started server on %s:%i" % (HOST, PORT)
+print "\nstarted server on %s:%i\n" % (HOST, PORT)
 
 
 while True:
@@ -16,7 +17,14 @@ while True:
     data = True 
     while data:
         try: 
-            data = client.recv(CHUNK).strip()
+            data = client.recv(CHUNK).rstrip()
+            print "DATA:", data
+            if data == "DONE":
+                client.close()
+                server.close()
+                print "EXITING service"
+                sys.exit(0)
+
             a, b = data.split(" ")
             r = global_align(a, b)
             client.send(" ".join(r))
