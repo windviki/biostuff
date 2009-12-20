8a335b7d2e008b25a2e60fa7e36a04daddb84b69
brentp <bpederse@gmail.com>
2009-12-20 11:41:40 -0800

    add commandline to short description.

--- pyfasta/setup.py
+++ pyfasta/setup.py
@@ -6,7 +6,7 @@ version = '0.3.5'
 setup(name='pyfasta',
       version=version,
       description=\
-        "fast, memory-efficient, pythonic access to fasta sequence files",
+        "fast, memory-efficient, pythonic (and command-line) access to fasta sequence files",
       url="http://bitbucket.org/brentp/biostuff/",
       long_description=open('README.txt').read() + "\n" + open('CHANGELOG.txt').read(),
       classifiers=["Topic :: Scientific/Engineering :: Bio-Informatics"],
