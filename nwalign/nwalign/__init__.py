64a2a0380a24d9c1bc1796b8315e042e529c7933
brentp <bpederse@gmail.com>
2009-12-26 17:31:22 -0800

    add commandline interface for the socket server.

--- nwalign/nwalign/__init__.py
+++ nwalign/nwalign/__init__.py
@@ -1,6 +1,55 @@
+"""
+------------------------------------------------------------------------------
+nwalign: fast `cython`_  - `Needleman-Wunsch`_ alignment
+------------------------------------------------------------------------------
+
+.. _`Needleman-Wunsch`: http://en.wikipedia.org/wiki/Needleman-Wunsch_algorithm 
+.. _`scoring matrix`: http://en.wikipedia.org/wiki/Substitution_matrix
+.. _`cython`: http://cython.org
+
+This module provides a python module and a command-line interface to do global-
+sequence alignment using the `Needleman-Wunsch` algorithm. It uses `cython`_ 
+and numpy for speed.
+
+Example Command-Line Usage 
+==========================
+
+
+the nwalign executable is installed to the PATH by setuptools
+::
+
+    $ nwalign alphabet alpet
+    alphabet
+    alp---et
+
+specify an alignment `scoring matrix`_ 
+::
+
+    $ nwalign --matrix /usr/share/ncbi/data/BLOSUM62 EEAEE EEEEG
+    EEAEE-
+    EE-EEG
+
+with specified penalties
+::
+
+    $ nwalign --gap_open -10 --gap_extend -4 --match 12 ASDFF ASFF
+    ASDFF
+    AS-FF
+
+
+Usage as a python module
+========================
+::
+
+    >>> import nwalign as nw
+    >>> nw.global_align("CEELECANTH", "PELICAN", matrix='PAM250')
+    ('CEELECANTH', '-PELICA--N')
+
+
+the matrix is specified as the full path to an `scoring matrix`_ as
+is distributed with the NCBI toolset.
+"""
 from cnwalign import global_align, global_align_no_matrix, score_alignment
-import cnwalign
-__doc__ = cnwalign.__doc__
 
 
 def main():
@@ -9,21 +58,27 @@ def main():
     parser = optparse.OptionParser(usage="""
     %prog [options] seq1 seq2 
     """)
-    parser.add_option("--gap", dest="gap", help="gap extend penalty (must be integer <= 0)", type="int", default=-1)
-    parser.add_option("--gap_init", dest="gap_init", help="gap start penalty (must be integer <= 0)", type="int", default=-1)
+    parser.add_option("--gap_extend", dest="gap_extend", help="gap extend penalty (must be integer <= 0)", type="int", default=-1)
+    parser.add_option("--gap_open", dest="gap_open", help="gap open penalty (must be integer <= 0)", type="int", default=-1)
     parser.add_option("--match", dest="match", help="match score (must be integer > 0)", type="int", default=1)
-    parser.add_option("--mismatch", dest="mismatch", help="gap penalty (must be integer < 0)", type="int", default=-1)
     parser.add_option("--matrix", dest="matrix", help="scoring matrix in ncbi/data/ format,\
                                       if not specificied, match/mismatch are used", default=None)
+    parser.add_option("--server", dest="server", default=0, type='int',
+                      help="if non-zero integer, a server is started")
 
     try:
         options, args = parser.parse_args()
     except:
         sys.exit(parser.print_help())
-    if len(args) != 2:
+    if options.server != 0:
+        import nwserver
+        nwserver.main(sys.argv[2])
+        
+    elif len(args) != 2:
         sys.exit(parser.print_help())
-    print "\n".join(global_align(args[0], args[1], options.gap, options.match,
-                                 options.mismatch, int(options.gap_init), options.matrix))
+    else:
+        print "\n".join(global_align(args[0], args[1], options.match,
+                                 options.gap_open, options.gap_extend, options.matrix))
 
 if __name__ == "__main__":
     main()
